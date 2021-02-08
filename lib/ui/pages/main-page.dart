f713d58e31c06ec1a95bb5b28d8cb4c72db332fc
fakhri ahnaf dhia <fakhri_ahnaf01@yahoo.co.id>
2021-02-09 00:36:16 +0700

    push config awal

--- lib/ui/pages/main-page.dart
+++ lib/ui/pages/main-page.dart
@@ -1,13 +1,23 @@
 part of 'pages.dart';
 
 class MainPage extends StatefulWidget {
+  final int initialPage;
+  MainPage({this.initialPage = 0});
+
   @override
   _MainPageState createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
   int selectedPage = 0;
-  PageController pageController = PageController(initialPage: 0);
+  PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedPage = widget.initialPage;
+    pageController = PageController(initialPage: widget.initialPage);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +30,7 @@ class _MainPageState extends State<MainPage> {
           SafeArea(
               child: Container(
             color: 'FAFAFC'.toColor(),
-          )), 
+          )),
           SafeArea(
               child: PageView(
             controller: pageController,
